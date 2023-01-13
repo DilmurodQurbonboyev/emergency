6ab168169bf239afa4ab8a86635f25534a0dc18d
dilmurodqurbonboyev <dilmurodqurbonboyev9820@gmail.com>
2023-01-13 11:49:35 +0500

    accordion removed

--- resources/views/frontend/regional.blade.php
+++ resources/views/frontend/regional.blade.php
@@ -60,36 +60,6 @@ $name = 'name_' . app()->getLocale();
                                         <img src="{{ asset('img/r-email.png') }}">
                                         <a href="#">{{ $leader->email ?? '' }}</a>
                                     </div>
-                                    <div class="accordion" id="i1">
-                                        <div class="accardion-nav">
-                                            <div class="accordion-header" id="headingOne">
-                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                                    Биография
-                                                </button>
-                                            </div>
-                                            <div class="accordion-header" id="headingTwo">
-                                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                                    Функциялари
-                                                </button>
-                                            </div>
-                                        </div>
-                                        <div class="accordion-item">
-                                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#i1">
-                                                <div class="accordion-body">
-                                                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="accordion-item">
-                                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#i1">
-                                                <div class="accordion-body">
-                                                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                                                </div>
-                                            </div>
-                                        </div>
-
-
-                                    </div>
                                 </div>
                             </div>
                         </div>
